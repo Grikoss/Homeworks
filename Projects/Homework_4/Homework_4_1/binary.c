503af69f41c3c547f329feaab6e5ad9641422d26
Grikoss <grikosru@mail.ru>
2020-10-13 22:02:05 +0300

    Complete binary.h and binary.c

--- Projects/Homework_4/Homework_4_1/binary.c
+++ Projects/Homework_4/Homework_4_1/binary.c
@@ -5,10 +5,43 @@
 #include "binary.h"
 
 void binarySummator(bool arrayOne[], bool arrayTwo[], bool resultArray[]) {
-
+	bool overFlow = 0;
+	int subSum = 0;
+	for (int i = 31; i >= 0; --i) {
+		subSum = arrayOne[i] + arrayTwo[i] + overFlow;
+		switch (subSum)
+		{
+		case 0:
+			resultArray[i] = 0;
+			overFlow = 0;
+			break;
+		case 1:
+			resultArray[i] = 1;
+			overFlow = 0;
+			break;
+		case 2:
+			resultArray[i] = 0;
+			overFlow = 1;
+			break;
+		case 3:
+			resultArray[i] = 1;
+			overFlow = 1;
+			break;
+		}
+	}
 }
 
 void binaryConverterOfExtraCode(bool array[]) {
+	for (int i = 0; i < 32; ++i) {
+			if (array[i]) {
+				array[i] = 0;
+			}
+			else
+			{
+				array[i] = 1;
+			}
+		}
+
 	bool additionalArray[32] = { 0 };
 	additionalArray[31] = 1;
 	bool storagearray[32] = { 0 };
@@ -31,7 +64,24 @@ void binaryUnpacker(int value, bool array[]) {
 
 int binaryPacker(bool array[]) {
 	int value = 0;
+	bool minus = false;
 	if (array[0]) {
 		binaryConverterOfExtraCode(array);
+		minus = true;
 	}
+
+	int powerOfTwo = 2147483648;
+	for (int i = 0; i < 32; ++i) {
+		if (array[i]) {
+			value += powerOfTwo;
+		}
+
+		powerOfTwo /= 2;
+	}
+
+	if (minus) {
+		return -value;
+	}
+
+	return value;
 }
