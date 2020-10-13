e00c78b4064f0d5e445e1fdaa1f6d51d9e123ee2
Grikoss <grikosru@mail.ru>
2020-10-13 22:51:18 +0300

    Complete

--- Projects/Homework_4/Homework_4_1/Homework_4_1.c
+++ Projects/Homework_4/Homework_4_1/Homework_4_1.c
@@ -11,7 +11,44 @@ void printfBoolArray(bool array[]) {
 	printf("\n");
 }
 
+bool isTestSuccessful(int valueOne, int valueTwo) {
+	bool binaryValueOne[32] = { 0 };
+	bool binaryValueTwo[32] = { 0 };
+	binaryUnpacker(valueOne, binaryValueOne);
+	binaryUnpacker(valueTwo, binaryValueTwo);
+	bool binaryResult[32] = { 0 };
+	binarySummator(binaryValueOne, binaryValueTwo, binaryResult);
+	return valueOne + valueTwo == binaryPacker(binaryResult);
+}
+
+void sistemTest(void) {
+	if (!isTestSuccessful(5, 10)) {
+		printf("Test failed\n");
+	}
+
+	if (!isTestSuccessful(-5, -26)) {
+		printf("Test failed\n");
+	}
+
+	if (!isTestSuccessful(9, -18)) {
+		printf("Test failed\n");
+	}
+
+	if (!isTestSuccessful(2147483648, 2147483648)) {
+		printf("Test failed\n");
+	}
+
+	if (!isTestSuccessful(-214748364, -214748364)) {
+		printf("Test failed\n");
+	}
+
+	if (!isTestSuccessful(-214748364, 2147483648)) {
+		printf("Test failed\n");
+	}
+}
+
 void main() {
+	sistemTest();
 	setlocale(LC_ALL, "Russian");
 	int valueOne = 0;
 	int valueTwo = 0;
@@ -28,4 +65,10 @@ void main() {
 	printfBoolArray(binaryValueOne);
 	printf("%i - ", valueTwo);
 	printfBoolArray(binaryValueTwo);
+	bool binaryResult[32] = { 0 };
+	binarySummator(binaryValueOne, binaryValueTwo, binaryResult);
+	int sum = binaryPacker(binaryResult);
+	printf("\n");
+	printf("Sum = %i - ", sum);
+	printfBoolArray(binaryResult);
 }
