b0781607b1d365c826056c1ac5b5ac8018474420
Grikoss <grikosru@mail.ru>
2020-11-16 15:10:18 +0300

    Small changes

--- Projects/Homework_5/MyList/list.h
+++ Projects/Homework_5/MyList/list.h
@@ -2,12 +2,17 @@
 
 typedef struct List List;
 
+//Создаёт новый стек
 List* createNewList(void);
 
+//Удаляет лист, принимает указатель на указатель
 int deleteList(List** list);
 
+//Добавляет элемент
 int addElement(List* list, char* inputItem);
 
+//Удаляет элемент
 int deleteElement(List* list);
 
+//Записывает количество элементов
 int getQuantity(int* quantity, List* list);
