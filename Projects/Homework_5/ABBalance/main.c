03df5003175d578c6a6d806b29aeb5f50a9312f8
Grikoss <grikosru@mail.ru>
2021-10-16 00:06:24 +0300

    add input

--- Projects/Homework_5/ABBalance/main.c
+++ Projects/Homework_5/ABBalance/main.c
@@ -1,6 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "../stack/stackTest.h"
 #include "ABBalanceCheckTest.h"
 #include "ABBalanceCheck.h"
+
+#define SIZE 30
 
 int main()
 {
@@ -14,5 +18,9 @@ int main()
         return 2;
     }
 
+    char string[SIZE] = { 0 };
+    printf("Enter string:\n");
+    scanf_s("%s", string, SIZE);
+    printf(AABalanceCheck(string, SIZE) ? "The balance of brackets is observed\n" : "The balance of brackets is not observed\n");
     return 0;
 }
