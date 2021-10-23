5b5fcc0968b7abf07aca86102275cf3a83b96f0d
Grikoss <grikosru@mail.ru>
2021-10-23 19:34:07 +0300

    add top function

--- Projects/Homework_5/stack/stack.h
+++ Projects/Homework_5/stack/stack.h
@@ -5,11 +5,18 @@ typedef struct Stack Stack;
 // Creates stack, returns pointer to stack
 Stack* createStack();
 
-// Creates new element, adds element into stack with value, returns execution code
+// Creates new element, adds element into stack with value, returns execution code, return execution code:
+// 0 - ok, 1 - stack is empty, 2 - memory error
 int push(Stack* stack, const int value);
 
-// Puts value into element, deletes current element, changes head to next, returns execution code
+// Puts value into element, deletes current element, changes head to next, if stack is empty don't change value, return execution code:
+// 0 - ok, 1 - stack is empty
 int pop(Stack* stack, int* value);
 
-// Delete all stack
+// Delete all stack, return execution code:
+// 0 - ok, 1 - stack is NULL
 int deleteStack(Stack* stack);
+
+// Puts value into element, don't delete current element, if stack is empty don't change value, return execution code:
+// 0 - ok, 1 - stack is empty
+int top(Stack* stack, int* value);
