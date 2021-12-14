6857f323a5b1b54bab00421b40a4d7b30e562563
Grikoss <grikosru@mail.ru>
2021-12-14 20:42:20 +0300

    it works

--- Projects/Homework_11/KMP/main.c
+++ Projects/Homework_11/KMP/main.c
@@ -1,4 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "kmp.h"
 #include "kmpTest.h"
+
+#define SIZE 100
+#define SIZE_OF_FILE 10000
 
 int main(int argc, char* argv[])
 {
@@ -9,5 +15,32 @@ int main(int argc, char* argv[])
     if (argc > 1)
     {
         return 0;
+    }
+    FILE* file = NULL;
+    fopen_s(&file, "input.txt", "r");
+    if (file == NULL)
+    {
+        return 1;
+    };
+    char* input = calloc(SIZE_OF_FILE, sizeof(char));
+    int index = 0;
+    while (!feof(file))
+    {
+        fscanf_s(file, "%c", &input[index], SIZE_OF_FILE);
+        ++index;
+    }
+    fclose(file);
+    char string[SIZE] = { 0 };
+    printf_s("Enter subString:\n");
+    scanf_s("%[^\n]", string, SIZE);
+    const int position = getPositionOfFirstOccurrence(string, input);
+    free(input);
+    if (position < 0)
+    {
+        printf_s("There are no occurrences\n");
+    }
+    else
+    {
+        printf_s("The position of the first occurrence is %d\n", position);
     }
 }
